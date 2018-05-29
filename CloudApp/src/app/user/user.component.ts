1eb10b78897aec15c71d72e31f640e2c2038f43e
Jasmien De Clercq <jasmien.declercq@hotmail.com>
2018-05-29 21:12:17 +0200

    Facebook login werkt ook, log uit knop toegevoegd

--- CloudApp/src/app/user/user.component.ts
+++ CloudApp/src/app/user/user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../core/user.service';
+import { AuthService } from '../core/auth.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+//import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirebaseUserModel } from '../core/user.model';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user: FirebaseUserModel = new FirebaseUserModel();
+  //profileForm: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    public userService: UserService,
+    public authService: AuthService,
+    private route: ActivatedRoute,
+    private location : Location,
+   // private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.route.data.subscribe(routeData => {
+      let data = routeData['data'];
+      if (data) {
+        this.user = data;
+        this.createForm(this.user.name);
+      }
+    })
+  }
+  /* createForm(name) {
+    this.profileForm = this.fb.group({
+      name: [name, Validators.required ]
+    });
+  } */
+
+  save(value){
+    this.userService.updateCurrentUser(value)
+    .then(res => {
+      console.log(res);
+    }, err => console.log(err))
   }
 
+  logout(){
+    this.authService.doLogout()
+    .then((res) => {
+      this.location.back();
+    }, (error) => {
+      console.log("Logout error", error);
+    });
+  }
 }
